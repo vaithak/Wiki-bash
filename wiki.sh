70f0ba13683c03922803e3579103d904d6fe9b3f
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-03-12 17:54:22 +0530

    Fixed color bug

--- wiki.sh
+++ wiki.sh
@@ -9,10 +9,11 @@ echo -e "Top 2 relevant searches\n"
 for i in `seq 0 $((length-1))`
 do
     output=`jq --argjson i "$i" '.[1][$i]' <<< $inp`
-    echo -e "\033[0:32m Title: \033[0;36m $output"
+    echo -e "\033[0;32m Title: \033[0;36m $output"
     output=`jq --argjson i "$i" '.[2][$i]' <<< $inp`
     echo -e "\033[0;32m About: \033[0;36m $output"
     output=`jq --argjson i "$i" '.[3][$i]' <<< $inp`
     echo -e "\033[0;32m Wiki Link: \033[0;36m $output\n"
 done
 
+echo -e "\033[0"
